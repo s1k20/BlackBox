be70dc0908208b4edc7a99271b9543699b59da81
Cian Boyle <cian.boyle@ucdconnect.ie>
2024-02-09 10:22:20 +0000

    fixed bug

--- Board.java
+++ Board.java
@@ -16,25 +16,21 @@ public class Board {
         initBoard();
     }
 
-    private void initBoard(){
-        for(int i = 0; i < HEIGHT; i++){
-            for(int j = 0; j < WIDTH; j++){
+    private void initBoard() {
+        for (int i = 0; i < HEIGHT; i++) {
+            for (int j = 0; j < WIDTH; j++) {
 
                 //conditions if i and j are at an index which could contain a ray marker
 
-                if(checkRayMarker(i, j)){
-
-                if(i + j == 5 || i + j == 15 || (i == 0 && j >= 5) ||
-                        (j == 10 && i < 6) || (i == 10 && j <= 5) || (j == 0 && i >= 5)){
-
+                if (checkRayMarker(i, j)) {
 
                     board[i][j] = new emptyMarker();
-
                 }
+
 
                 //conditions if i and j are at indexes which are not part of the board
                 //but present in order to represent the board
-                else if(i + j <= 4 || i + j > 15){
+                else if (i + j <= 4 || i + j > 15) {
 
                     board[i][j] = new nullHex();
                 }
@@ -42,6 +38,7 @@ public class Board {
         }
 
     }
+
 
     public void placeAtom(int x, int y){
         Atom newAtom = new Atom(x, y);
@@ -81,24 +78,26 @@ public class Board {
     }
 
     private static boolean checkRayMarker(int i, int j){
-        return i + j == 5 || i + j == 15 || (i == 0 && j >= 5) ||
-                (j == 10 && i < 6) || (i == 10 && j <= 5) || (j == 0 && i >= 5);
-        for(int i = 0; i < HEIGHT; i++){
-            for(int j = 0; j < WIDTH; j++){
-                if(board[i][j] instanceof nullHex){
-                    System.out.print(" ");
-                }
-                else if(board[i][j] instanceof emptyMarker){
-                    System.out.print("o");
-                }
-                else{
-                    System.out.print("x");
-                }
-            }
-            System.out.println();
-        }
-
+            return i + j == 5 || i + j == 15 || (i == 0 && j >= 5) ||
+                    (j == 10 && i < 6) || (i == 10 && j <= 5) || (j == 0 && i >= 5);
     }
+
+//        for(int i = 0; i < HEIGHT; i++){
+//            for(int j = 0; j < WIDTH; j++){
+//                if(board[i][j] instanceof nullHex){
+//                    System.out.print(" ");
+//                }
+//                else if(board[i][j] instanceof emptyMarker){
+//                    System.out.print("o");
+//                }
+//                else{
+//                    System.out.print("x");
+//                }
+//            }
+//            System.out.println();
+//        }
+
+
 
     private static class nullHex{
     }
--- Game.java
+++ Game.java
@@ -14,6 +14,4 @@ public class Game {
         board.printTempBoard();
     }
 
-
-    }
 }
