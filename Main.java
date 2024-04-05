8ba17c9eedde8714c96436180e5edd6a91abda62
Cian Boyle <cian.boyle@ucdconnect.ie>
2024-04-05 14:54:59 +0100

    added previously guessed atoms list

--- Main.java
+++ Main.java
@@ -4,7 +4,7 @@ public class Main {
     public static void main(String[] args) {
         //run game
         Game game = new Game();
-//        game.playGame();
-        game.testGame();
+        game.playGame();
+//        game.testGame();
     }
 }
